36fa0f76de41c0386e7e3825040681ca66c702fb
Mohit Yadav <my266620@gmail.com>
2026-01-04 19:20:47 +0530

    /

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,35 @@
+import dotenv from "dotenv";
+dotenv.config();
 
 import express from "express";
-import dotenv from "dotenv";
+import mongoose from "mongoose";
 import cors from "cors";
-import authRoutes from "./routes/authRoutes.js";
-import connectDB from "./config/db.js";
+import path from "path";
 
-import contactRoutes from "./routes/contactRoutes.js";
 import appointmentRoutes from "./routes/appointmentRoutes.js";
-import serviceRoutes from "./routes/serviceRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
-
-
-
-dotenv.config();
-connectDB();
+import serviceRoutes from "./routes/serviceRoutes.js";
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// ✅ make uploads folder public
+app.use("/uploads", express.static("uploads"));
+
 app.use("/api/auth", authRoutes);
-app.use("/api/contact", contactRoutes);
-app.use("/api/appointment", appointmentRoutes);
-app.use("/api/services", serviceRoutes);
+app.use("/api/appointments", appointmentRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/services", serviceRoutes);
 
-app.get("/", (req, res) => res.send("TailorHub Backend Running..."));
+mongoose
+  .connect("mongodb://127.0.0.1:27017/tailorhub")
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`));
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
